e0c002c170d2ef0c6bfdd475b176508df244cb3d
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-05-31 11:47:24 -0500

    Agregado de primera entidad

---
+++ GEMAF/Modelos/Entidades/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEMAF.Modelos.Entidades
+{
+    class Articulo
+    {
+        private int idArticulo;
+        private string nombre;
+        private string estado;
+        private string clase;
+
+        public int IdArticulo { get => idArticulo; set => idArticulo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Clase { get => clase; set => clase = value; }
+    }
+}
